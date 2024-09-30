dc93f13b66153d641fc407e713c4c5869c1876e4
Flo <cappelflo@gmail.com>
2024-09-30 18:55:58 +0200

    Création du dossier components puis modif app.js les fichier contact, etc ne sont pas encore crée mais les <routes> oui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import ContactForm from './components/ContactForm';
+import CardGrid from './components/CardGrid';
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/contact">Formulaire de contact</Link></li>
+            <li><Link to="/cards">Grille de cartes</Link></li>
+            <li><Link to="/counter">Compteur de clics</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/contact" element={<ContactForm />} /> 
+          <Route path="/cards" element={<CardGrid />} />
+          <Route path="/counter" element={<ClickCounter />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
