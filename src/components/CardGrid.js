257d2f8bbb74b94628eab64a80deb9a065793428
Flo <cappelflo@gmail.com>
2024-09-30 19:28:14 +0200

    Pas encore mais bientot les images fonctionnel

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import image1 from'../images/Exoplanète-K2-18-b-–-NIRISS.jpg';
+import image2 from '../images/Galaxie-de-la-Roue-de-chariot-–-Image-du-NIRCam-et-du-MIRI.jpg';
+import image3 from '../images/alien_style.jpg';
+import image4 from '../images/c8d5f96adf4135e0e9353d7e95cb40c6.png';
 
-function Card() {  // peut être mettre export avant mettre style et autrte info dans ()
-  return <div style={{ border: '1px solid black', padding: '20px', margin: '10px' }}>Carte</div>;
+function Card(image) {  // ajout de "propriété" image
+  return <div style={{ border: '1px solid black', padding: '20px', margin: '10px' }}>
+    <img src={image} alt="Card" style={{ width: '100%', height: 'auto' }} />
+  <p>Carte</p>
+</div>;
 }
 
 function CardGrid() {
